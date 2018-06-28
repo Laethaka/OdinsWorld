02cd7e0de6df37c484667956485e73efe60c9870
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-06-28 14:04:41 -0700

    puted background img

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 const Jumbotron = ({ children }) => (
-  <div
-    style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-    
-  >
+  <div>
     {children}
   </div>
 );
