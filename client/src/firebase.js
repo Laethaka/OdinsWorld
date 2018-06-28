817a5a59953333a952853c3f1c995e01dbd5ede5
Jesse <laethaka@gmail.com>
2018-06-28 15:03:15 -0700

    pulling from maria

---
+++ client/src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase'
+const config = {
+    apiKey: "AIzaSyDpiMBX08UvfF3AbcqR0Dc9vmp747c4-rw",
+    authDomain: "odinsworld-99dca.firebaseapp.com",
+    databaseURL: "https://odinsworld-99dca.firebaseio.com",
+    projectId: "odinsworld-99dca",
+    storageBucket: "odinsworld-99dca.appspot.com",
+    messagingSenderId: "260576085470"
+  };
+  firebase.initializeApp(config);
+export default firebase;
