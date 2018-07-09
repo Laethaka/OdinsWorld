ecc4e4e4aaa148c4c4af0ea6cec804840d2d4893
M Jesús Otero <mjesuotero@Maria-Jesus-Otero-R-T.local>
2018-07-08 18:05:33 -0700

    x

--- client/src/components/LobbyGame/LobbyGame.js
+++ client/src/components/LobbyGame/LobbyGame.js
@@ -3,24 +3,23 @@ import './LobbyGame.css';
 
 const LobbyGame = (props) => (
 
-    <div className="gamesAvail">
+    <div className="join-game-rectangle">
         <div className="row">
             <div className="col-sm-12">
-                <h3>Players in game</h3>
+                <h3 >Players in game</h3>
             </div>
         </div>
 
         <div className="row">
-            <div className="col-sm-9">
+            <div className="col-sm-10">
                 <p className="margin-left">– {props.players[0]}</p>
                 <p className="margin-left">–{props.players[1]}</p>
             </div>
         
-            <div className="col-sm-3">
+            <div className="col-sm-2">
                 <a type="btn" className="btn button button-join" id="joinButton" href={"/game/" + (props.id + 1) }>Join</a>
             </div>  
         </div>
-        <hr className="style-one" />
     </div>
 
 )
