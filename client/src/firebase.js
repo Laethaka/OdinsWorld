9c4e689858e7c52037436106583fcf0f073ff2ad
Mosjoandy <34078510+Mosjoandy@users.noreply.github.com>
2018-07-12 16:47:36 -0700

    hide API keys

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,11 +1,11 @@
 import firebase from 'firebase'
 const config = {
-  apiKey: "AIzaSyDpiMBX08UvfF3AbcqR0Dc9vmp747c4-rw",
-  authDomain: "odinsworld-99dca.firebaseapp.com",
-  databaseURL: "https://odinsworld-99dca.firebaseio.com",
-  projectId: "odinsworld-99dca",
-  storageBucket: "odinsworld-99dca.appspot.com",
-  messagingSenderId: "260576085470"
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASEURL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
 firebase.initializeApp(config);
 export const provider = new firebase.auth.GoogleAuthProvider();
